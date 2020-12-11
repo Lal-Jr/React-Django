c13a04b2d9fa4ecba3415a923bc52097484638fd
Harish Lal <harishlal80@gmail.com>
2020-12-11 22:55:59 +0530

    Changes
    Addition in Urls.py of Api folder and some new additions in the .js files of the frontend folder

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormerHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component{
     constructor(props) {
@@ -6,7 +16,23 @@ export default class CreateRoomPage extends Component{
     }
 
     render() {
-        return <p>This is CreateRoomPage</p>
+        return (<Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component="h4" variant="h4">
+                    Create a Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl component="fieldset">
+                    <FormHelperText>
+                        <div align="center">
+                            Guest Control of Playback State
+                        </div>
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+        </Grid>
+        );
     }
 }
 
